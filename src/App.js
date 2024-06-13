af33e1c8e5ba1b4c55288fb69ecce7a8f83139f5
djamalB <djamal.balaev@bk.ru>
2024-06-13 10:22:09 +0300

    сделал светофор

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import { Routes, Route } from "react-router-dom";
-import Root from "./Root";
-import "./index.css";
+import React, { useEffect, useState } from "react";
+import "./footer.css";
 
 function App() {
+  const [light, setLight] = useState("red");
+
+  useEffect(() => {
+    const colorChange = () => {
+      setLight((prev) => {
+        if (prev === "red") return "yellowAfterRed";
+        if (prev === "yellowAfterRed") return "green";
+        if (prev === "green") return "yellowAfterGreen";
+        if (prev === "yellowAfterGreen") return "red";
+      });
+    };
+
+    let interval;
+    if (light === "red" || light === "green") {
+      interval = setInterval(colorChange, 4000);
+    } else {
+      interval = setInterval(colorChange, 1000);
+    }
+
+    return () => clearInterval(interval);
+  }, [light]);
+
   return (
-    <Routes>
-      <Route path="/" element={<Root />} />
-    </Routes>
+    <div className="root">
+      <div className="block">
+        <div
+          className={`light red ${light.startsWith("red") ? "active" : ""}`}
+        ></div>
+        <div
+          className={`light yellow ${
+            light.startsWith("yellow") ? "active" : ""
+          }`}
+        ></div>
+        <div
+          className={`light green ${light.startsWith("green") ? "active" : ""}`}
+        ></div>
+      </div>
+    </div>
   );
 }
+
 export default App;
